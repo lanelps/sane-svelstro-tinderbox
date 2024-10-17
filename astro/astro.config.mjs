5581bca8e2b850812da444585b9dba93f9abd75d
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-10-17 13:14:56 +1100

    Add Astro configuration files and folders

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,35 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
+
+import { loadEnv } from "vite";
+const env = {
+  ...process.env,
+  ...loadEnv(process.env.NODE_ENV, process.cwd(), ["SANITY_"]),
+};
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sane-svelstro-tinderbox.com",
+  integrations: [
+    svelte(),
+    tailwind(),
+    sanity({
+      projectId: env.SANITY_PROJECT_ID,
+      dataset: env.SANITY_DATASET,
+      token: env.SANITY_TOKEN,
+      useCdn: false,
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  prefetch: true,
+});
