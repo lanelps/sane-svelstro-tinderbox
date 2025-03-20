dee6626085c269f0fce9a4c4819a86ba43d50929
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-03-20 16:35:36 +1100

    Refactor Tailwind CSS integration to v4

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sanity from "@sanity/astro";
-
 import { loadEnv } from "vite";
+
 const env = {
   ...process.env,
   ...loadEnv(process.env.NODE_ENV, process.cwd(), ["SANITY_"]),
@@ -17,7 +17,6 @@ export default defineConfig({
   site: "https://sane-svelstro-tinderbox.com",
   integrations: [
     svelte(),
-    tailwind(),
     sanity({
       projectId: env.SANITY_PROJECT_ID,
       dataset: env.SANITY_DATASET,
@@ -31,6 +30,12 @@ export default defineConfig({
       },
     }),
   ],
-  prefetch: true,
   output: "static",
+  prefetch: {
+    prefetchAll: true,
+  },
+  vite: {
+    assetsInclude: ["**/*.glsl"],
+    plugins: [tailwindcss()],
+  },
 });
