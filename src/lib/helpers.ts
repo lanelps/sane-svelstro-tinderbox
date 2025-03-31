1cea27d504b36213028fd02fdf1a0427c235d98c
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-03-31 13:22:02 +1100

    Change keywords in getPageSeo to default to an empty array for improved data handling

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,7 +4,7 @@ export const getPageSeo = (page: PageTypes) => {
   const seo = {
     title: page?.title || page?.seo?.title || "",
     description: page?.seo?.description || "",
-    keywords: page?.seo?.keywords || "",
+    keywords: page?.seo?.keywords || [],
     image: page?.seo?.image || "",
   };
 
