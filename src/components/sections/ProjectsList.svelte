14ba53347a1b97f7a843a5fef9a812d514678d6c
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-04-01 13:50:40 +1100

    Refactor ProjectsList component to use Link component for project links

--- src/components/sections/ProjectsList.svelte
+++ src/components/sections/ProjectsList.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
+  import Link from "@components/Link.svelte";
   import Media from "@components/Media.svelte";
-  import type { ProjectsListSection } from "@/types";
+  import type { ProjectsListSection, InternalLink } from "@/types";
 
   interface Props {
     data: ProjectsListSection;
   }
 
   const { data }: Props = $props();
+
+  $inspect(data.projects);
 </script>
 
 <section>
   <ul class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
     {#each data.projects as project}
+      {@const projectLink: InternalLink = {
+        type: "internal",
+        label: "",
+        reference: {
+          _id: project._id,
+          _type: "project",
+          slug: { current: project.slug.current },
+          title: project.title,
+        },
+      }}
+
       <li class="rounded-lg border border-gray-200 p-4">
-        <h2 class="text-h2">{project.title}</h2>
-        <p>{project.date}</p>
-        <Media media={project.thumbnail} />
+        <Link link={projectLink}>
+          <h2 class="text-h2">{project.title}</h2>
+          <p>{project.date}</p>
+          <Media media={project.thumbnail} />
+        </Link>
       </li>
     {/each}
   </ul>
