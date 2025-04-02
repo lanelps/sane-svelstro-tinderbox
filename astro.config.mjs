f72ecae4c5bf8f43b8616e58f785b35d061f2cc9
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-04-02 15:31:00 +1100

    Refactor Astro configuration to use import.meta.env for Sanity project ID and dataset

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,8 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import sanity from "@sanity/astro";
-import { loadEnv } from "vite";
 
 import cloudflare from "@astrojs/cloudflare";
-
-const env = {
-  ...process.env,
-  ...loadEnv(process.env.NODE_ENV, process.cwd(), ["PUBLIC_SANITY_"]),
-};
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,8 +26,8 @@ export default defineConfig({
 
   integrations: [
     sanity({
-      projectId: env.PUBLIC_SANITY_PROJECT_ID,
-      dataset: env.PUBLIC_SANITY_DATASET,
+      projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+      dataset: import.meta.env.PUBLIC_SANITY_DATASET,
       useCdn: false,
       apiVersion: "2025-03-21", // insert the current date to access the latest version of the API
       studioBasePath: "/admin", // If you want to access the Studio on a route
