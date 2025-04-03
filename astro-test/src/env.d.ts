b9a9135d0c70c9171df05ce22787b51c8f2de6f6
Lane Le Prevost-Smith <lanelps@gmail.com>
2025-04-03 12:44:10 +1100

    Remove unused Runtime type and related namespace from env.d.ts

--- astro-test/src/env.d.ts
+++ astro-test/src/env.d.ts
@@ -1,8 +1,2 @@
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
-
-declare namespace App {
-  interface Locals extends Runtime {}
-}
-
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
