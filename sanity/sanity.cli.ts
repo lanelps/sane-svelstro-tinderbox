ff3ae8bb31582345c38ce6e05bf24c3048d8eb9b
Lane Le Prevost-Smith <lanelps@gmail.com>
2026-04-08 15:15:14 +1000

    Refactor Sanity CLI configuration: streamline projectId and dataset assignment, update deployment settings

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,13 +1,13 @@
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID!;
-const dataset = process.env.SANITY_STUDIO_STUDIO_DATASET!;
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
+const dataset = process.env.SANITY_STUDIO_STUDIO_DATASET!
 
 export default defineCliConfig({
   api: {
     projectId,
-    dataset
+    dataset,
   },
-  autoUpdates: true,
-  studioHost: "boilerplate",
+  deployment: {autoUpdates: true},
+  studioHost: 'boilerplate',
 })
